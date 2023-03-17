6eef474a17a1a9596c8347fb9ff4a351fa50bbb0
mariam zabakhidze <mariamzabakhidze@gmail.com>
2023-03-17 18:52:35 +0400

    add new VS solution

---
+++ introtovisualstudioweek4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace introtovisualstudioweek4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter some text");
+            var someword = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(someword + "Added value");
+            Console.ResetColor();
+
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine("/___|");
+            Console.WriteLine(" /  |");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
